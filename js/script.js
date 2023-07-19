ee3666f99fd4b6c8abfb296d2c923d1da39e9630
Piotr Kania <pkania@web-coders.pl>
2023-07-19 10:58:24 +0200

    initial commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const btn = document.querySelector('button');
+const h1 = document.querySelector('h1');
+
+let counter = 0;
+
+btn.addEventListener('click', ()=>{
+    
+    counter++;
+
+    if(counter % 2 === 0) {
+        h1.innerText = 'Salon samochodowy w Krakowie';
+    } else {
+        h1.innerText = 'Piotr Kania';
+    }
+
+
+});
