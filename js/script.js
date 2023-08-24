d7bdd0b74496cca5e768f77f51a5701a214ba550
Tymoteusz Bartos <tymekbartos08@gmail.com>
2023-08-24 11:46:14 +0200

    add grid

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const btn = document.querySelector('button');
+const btn = document.querySelector('.btn-header');
 const h1 = document.querySelector('h1');
 
 let counter = 0;
@@ -15,5 +15,23 @@ btn.addEventListener('click', ()=>{
         h1.innerText = 'Tymoteusz Bartos';
     }
 
+
+});
+
+const hamburger = document.querySelector('.top .material-icons');
+const closeBtn = document.querySelector('nav .material-icons');
+const nav = document.querySelector('nav');
+
+hamburger.addEventListener('click', () => {
+
+    nav.classList.add('display-block')
+    // nav.style.display = 'block';
+
+});
+
+closeBtn.addEventListener('click', () => {
+
+    nav.classList.remove('display-block')
+    // nav.style.display = 'none';
 
 });
